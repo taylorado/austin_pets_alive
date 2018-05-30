54022dd11f4588622da3bb010d6291c2e9cb8620
Taylor <33101225+taylorado@users.noreply.github.com>
2018-05-30 12:51:13 -0500

    nokogiri implemented for index page. detail paragraph parsed into array
    todo:
    
    -reduce number of dogs visible at one time in CLI (currently ALL)
    -scrape detail pages for each dog

--- lib/austin_pets_alive/dogs.rb
+++ lib/austin_pets_alive/dogs.rb
@@ -14,7 +14,8 @@ attr_accessor :name, :breed, :age, :url
     dog_index.css("li.pet").each do |dog|
       current_dog = self.new
       current_dog.name = dog.css("a").attribute("title").value
-      current_dog.age = dog.at('p:contains("ale")').text.strip 
+      @details = dog.at('p:contains("ale")').text.strip.split(/\n/)
+      current_dog.age = @details[3].to_s
       current_dog.breed = "Dog"
       current_dog.url = dog.css("a").attribute("href").value
       scraped_dogs << current_dog
