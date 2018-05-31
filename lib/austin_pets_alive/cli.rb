a8a03496fa4cbca62eac2aa15581c7396aaa9e28
Taylor <33101225+taylorado@users.noreply.github.com>
2018-05-31 14:20:42 -0500

    fixed errors when we run out of dogs at the end of the list

--- lib/austin_pets_alive/cli.rb
+++ lib/austin_pets_alive/cli.rb
@@ -17,11 +17,12 @@ class AustinPetsAlive::CLI
   end
 
   def list_dogs
-    puts "10 dogs displayed per page."
+    puts "10 dogs displayed per page.  Showing dogs #{@@start_point} to #{@@start_point + 10}"
     @dog_page = AustinPetsAlive::Dogs.all[@@start_point, 10]
     @dog_page.each.with_index(1) do |dog, i|
       puts "#{i}. '#{dog.name}' - #{dog.breed} - #{dog.age} old"
     end
+    puts "Enter the number of the dog you would like more information on.  Type 'next' to see the next 10 dogs.  Type 'exit' to exit"
 
 
   end
@@ -30,20 +31,24 @@ class AustinPetsAlive::CLI
     # more information on one of these dogs?  or move on to next set of most recent dogs?
     input = nil
     while input != 'exit'
-      puts "Enter the number of the dog you would like more information on.  Type 'next' to see the next 10 dogs.  Type 'exit' to exit"
       input = gets.strip.downcase
-
       if input.to_i > 0 && input.to_i <= 10
         AustinPetsAlive::Dogs.show_dog(input.to_i - 1 + @@start_point)
-      elsif input == 'next'
+      elsif input == 'next' || input == 'n'
         @@start_point += 10
-        list_dogs
+        if @@start_point < AustinPetsAlive::Dogs.all.count
+          list_dogs
+        else
+          puts "There are no more dogs!  Type 'exit' to exit."
+
+        end
 
       elsif input != 'exit'
         puts 'Invalid input. Try Again.'
 
       end
     end
+
   end
 
   def goodbye
